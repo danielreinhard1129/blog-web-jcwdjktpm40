5c061f1b3269762cd595eeb6e9e5bc8679b576ce
danielreinhard1129 <danielreinhard1129@gmail.com>
2026-04-08 15:56:13 +0700

    feat: add register feature

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: "https://securepies-us.backendless.app/api",
 });
-
 // bikir84466@iaciu.com
 // Admin123
+
+export const axiosInstance2 = axios.create({
+  baseURL: "http://localhost:8000",
+});
